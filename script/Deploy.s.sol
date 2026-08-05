3ef51f6d32fc6b32f1207134fd9ee6d9c46575d3
fzn-sh <aderhmfzn@gmail.com>
2026-08-05 22:13:46 +0700

    style(script): format Deploy.s.sol

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -10,10 +10,8 @@ import {FlashLiquidationReceiver} from "../src/FlashLiquidationReceiver.sol";
 
 contract DeployScript is Script {
     function run() external {
-        uint256 deployerPrivateKey = vm.envOr(
-            "PRIVATE_KEY",
-            uint256(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80)
-        );
+        uint256 deployerPrivateKey =
+            vm.envOr("PRIVATE_KEY", uint256(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80));
 
         address deployer = vm.addr(deployerPrivateKey);
 
